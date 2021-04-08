6f35784b84352ae82a1c2d499e08c1c21789e1ae
marnixteunissen <70704360+marnixteunissen@users.noreply.github.com>
2021-04-08 09:03:01 +0200

    added new dataset without too long file nam

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ https://app.roboflow.com/dataset/pipe-detection
 transfer learning command:
 
 python train_transfer.py --batch 8 --epochs 2 --data ../pipe_det_data_07_04_21/data.yaml --cfg models/yolov5s.yaml --weights yolov5s.pt --name yolov5s_pip_det
+
+python train_transfer.py --batch 8 --epochs 100 --data ../pipe_det_data_07_04_21/data.yaml --cfg models/yolov5x.yaml --weights yolov5x.pt --name yolov5x_pip_det
--- transfer_learning.py
+++ transfer_learning.py
@@ -1,11 +1,11 @@
-#!/usr/bin/env python3
 import torch
-import train_transfer
 import os
 import sys
 
 current_dir = os.path.dirname(__file__)
 yolo_dir = os.path.join(current_dir, 'yolov5')
+print(yolo_dir)
 sys.path.append(yolo_dir)
-os.system('python train_transfer.py --batch 8 --epochs 2 --data ../pipe_det_data_07_04_21/data.yaml --cfg models/yolov5s.yaml --weights yolov5s.pt --name yolov5s_pip_det')
+print(sys.path)
+os.system('python train_transfer.py --batch 8 --epochs 2 --data ../pipe_det_data_08_04_21/data.yaml --cfg models/yolov5s.yaml --weights yolov5s.pt --name yolov5s_pip_det')
 
